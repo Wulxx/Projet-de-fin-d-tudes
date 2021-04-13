af60da59e6bfd33189fd0add975ac26d395a2605
yodelvaux <yodelvaux@ITEM-S86338.emea.msad.sopra>
2021-04-13 14:20:38 +0200

    Premier push du projet

---
+++ Test Suites/TestSuiteForInput.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteForInput</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1eca6d44-11e0-4be4-bfaf-fed3bcaa7322</testSuiteGuid>
+   <testCaseLink>
+      <guid>660803c0-372f-43c6-b31d-9aca8854f74a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DDT/Input</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>7f4f578c-2e01-41d9-8c98-46552ed0ad13</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Input/Input_Reference</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>7f4f578c-2e01-41d9-8c98-46552ed0ad13</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Recherche</value>
+         <variableId>2d7dc608-dd64-47a2-8e4b-99b6830e7c20</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
